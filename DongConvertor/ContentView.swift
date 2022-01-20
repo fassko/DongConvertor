49882ff3cdd376ac599021cb52a00f0c92465d88
Kristaps Grinbergs <fassko@gmail.com>
2022-01-20 20:59:58 +0200

    Simplify UI

--- DongConvertor/ContentView.swift
+++ DongConvertor/ContentView.swift
@@ -13,48 +13,40 @@ struct ContentView: View {
   
   var body: some View {
     VStack(spacing: 0) {
-      ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
-        Color.red
-          .opacity(0.3)
-          .edgesIgnoringSafeArea(.all)
-        
-        VStack {
-          Spacer()
-          
-          HStack(spacing: 20) {
-            Image("vietnam-flag")
-              .resizable()
-              .scaledToFit()
-              .frame(height: 80)
-            
-            VStack(alignment: .leading) {
-              Text("VDN")
-                .font(.largeTitle.weight(.semibold))
-              
-              Text("Vietnamese Dong")
-                .font(.title3)
-              
-              TextField("VDN", text: $vietnameseDong)
-                .font(.largeTitle)
-                .minimumScaleFactor(0.2)
-                .frame(minHeight: 45)
-                .foregroundColor(.black)
-                .keyboardType(.decimalPad)
-            }
-            .foregroundColor(.black)
-          }
-          .padding(.bottom, 60)
-          .padding(.horizontal)
-        }
-      }
       
-      ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
-        Color.yellow
-          .opacity(0.8)
-          .edgesIgnoringSafeArea(.all)
+      VStack {
+        Spacer()
         
+        HStack(spacing: 20) {
+          Image("vietnam-flag")
+            .resizable()
+            .scaledToFit()
+            .frame(height: 80)
+          
+          VStack(alignment: .leading) {
+            Text("VDN")
+              .font(.largeTitle.weight(.semibold))
+            
+            Text("Vietnamese Dong")
+              .font(.title3)
+            
+            TextField("VDN", text: $vietnameseDong)
+              .font(.largeTitle)
+              .minimumScaleFactor(0.2)
+              .frame(minHeight: 45)
+              .foregroundColor(.black)
+              .keyboardType(.decimalPad)
+          }
+        }
+        .padding(.bottom, 60)
+        .padding(.horizontal)
+      }
+      .background(Color.red.opacity(0.3))
+      
+      VStack {
         HStack {
           Spacer()
+          
           Button(action: convert) {
             Image(systemName: "equal")
               .font(.system(size: 60))
@@ -67,7 +59,7 @@ struct ContentView: View {
         .padding(.top, -30)
         .padding(.horizontal)
         
-        VStack {
+        HStack {
           HStack(spacing: 20) {
             Image("eu-flag")
               .resizable()
@@ -87,15 +79,16 @@ struct ContentView: View {
                 .frame(minHeight: 45)
                 .foregroundColor(.black)
                 .keyboardType(.decimalPad)
+                .disabled(true)
             }
-            .foregroundColor(.black)
           }
-          
-          Spacer()
         }
         .padding(.top, 60)
         .padding(.horizontal)
+        
+        Spacer()
       }
+      .background(Color.yellow.opacity(0.8))
     }
   }
   
